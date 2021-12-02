250620410104c8ab5cfef35db7f7c0258f537ce2
姚威 <yaowei@yaoweideMacBook-Pro.local>
2021-12-02 11:26:43 +0800

    服务限流调整

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 	github.com/golang/protobuf v1.5.2
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/registry/consul/v2 v2.9.1
+	github.com/micro/go-plugins/wrapper/breaker/hystrix/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/wrapper/ratelimiter/ratelimit/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2 v2.9.1 // indirect
 	github.com/spf13/cast v1.3.0
 	github.com/spf13/viper v1.6.3
 	google.golang.org/protobuf v1.27.1
--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	"blog/bootstrap"
 	"blog/handler"
 	blog "blog/proto/blog"
+	limit "github.com/juju/ratelimit"
 	"github.com/micro/go-micro/v2"
 	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-plugins/registry/consul/v2"
+	"github.com/micro/go-plugins/wrapper/ratelimiter/ratelimit/v2"
 )
 
 func main() {
 	bootstrap.Initialization()
 	consul := consul.NewRegistry()
+	rateLimit := limit.NewBucketWithRate(float64(500), int64(1000))
 	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.service.blog"),
 		micro.Version("latest"),
 		micro.Registry(consul),
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(rateLimit,false)),
 	)
+	service.Init()
+
 	// Register Handler
 	blog.RegisterBlogHandler(service.Server(), new(handler.Blog))
 
